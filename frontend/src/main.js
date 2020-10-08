7fa1ec59a21453762b5b51e75836de5b9cab7930
JoseContreras <jocontrera16@gmail.com>
2020-10-08 01:16:18 -0300

    front ready to test

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 import Vue from 'vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import App from './App.vue';
 import router from './router';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+// setting axios
+axios.defaults.baseURL = process.env.VUE_APP_DEV_API_HOST;
+Vue.use(VueAxios, axios);
 
 // Install BootstrapVue
 Vue.use(BootstrapVue);
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -36,6 +36,7 @@
           :filter="filter"
           :per-page="perPage"
           :fields="fields"
+          @filtered="onFiltered"
           striped
           hover
         >
@@ -85,14 +86,70 @@
 
       </b-row>
 
-      <!-- Modals -->
+      <!-- Modals - Separar en componentes -->
       <b-modal
         id="modal-create"
         title="Crear una tripulación"
         @hidden="cleanCrew()"
         @ok="createCrew()"
       >
-        <p class="my-4">Hello from create!</p>
+        <b-form-group
+          id="create-1"
+          label="Nombre de tripulación"
+          label-for="create-input-1"
+        >
+          <b-form-input id="create-input-1" v-model="crew.name" type="text" required></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="create-2"
+          label="Cantidad de tripulantes"
+          label-for="create-input-2"
+        >
+          <b-form-input id="create-input-2"
+            v-model="crew.crew_quantity"
+            type="number"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="create-3"
+          label="Modelo de la nave"
+          label-for="create-input-3"
+        >
+          <b-form-input
+            id="create-input-3"
+            v-model="crew.ship_name"
+            type="text"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="create-4"
+          label="Costo de la nave"
+          label-for="create-input-4"
+        >
+          <b-form-input id="create-input-4"
+            v-model="crew.ship_cost"
+            type="number"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="create-5"
+          label="Velocidad máxima de la nave"
+          label-for="create-input-5"
+        >
+          <b-form-input id="create-input-5"
+            v-model="crew.ship_max_speed"
+            type="number"
+            required
+          ></b-form-input>
+        </b-form-group>
+
       </b-modal>
 
       <b-modal
@@ -100,7 +157,63 @@
         title="Detalles de tripulación"
         @hidden="cleanCrew()"
       >
-        <p class="my-4">Hello from details!</p>
+        <b-form-group
+          id="details-1"
+          label="Nombre de tripulación"
+          label-for="details-input-1"
+        >
+          <b-form-input id="details-input-1"
+            v-model="crew.name" type="text" disabled></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="details-2"
+          label="Cantidad de tripulantes"
+          label-for="details-input-2"
+        >
+          <b-form-input id="details-input-2"
+            v-model="crew.crew_quantity"
+            type="number"
+            disabled
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="details-3"
+          label="Modelo de la nave"
+          label-for="details-input-3"
+        >
+          <b-form-input
+            id="details-input-3"
+            v-model="crew.ship_name"
+            type="text"
+            disabled
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="details-4"
+          label="Costo de la nave"
+          label-for="details-input-4"
+        >
+          <b-form-input id="details-input-4"
+            v-model="crew.ship_cost"
+            type="number"
+            disabled
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="details-5"
+          label="Velocidad máxima de la nave"
+          label-for="details-input-5"
+        >
+          <b-form-input id="details-input-5"
+            v-model="crew.ship_max_speed"
+            type="number"
+            disabled
+          ></b-form-input>
+        </b-form-group>
       </b-modal>
 
       <b-modal
@@ -109,7 +222,59 @@
         @hidden="cleanCrew()"
         @ok="updateCrew()"
       >
-        <p class="my-4">Hello from edit!</p>
+        <b-form-group
+          id="update-1"
+          label="Nombre de tripulación"
+          label-for="update-input-1"
+        >
+          <b-form-input id="update-input-1"
+            v-model="crew.name" type="text" ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="update-2"
+          label="Cantidad de tripulantes"
+          label-for="update-input-2"
+        >
+          <b-form-input id="update-input-2"
+            v-model="crew.crew_quantity"
+            type="number"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="update-3"
+          label="Modelo de la nave"
+          label-for="update-input-3"
+        >
+          <b-form-input
+            id="update-input-3"
+            v-model="crew.ship_name"
+            type="text"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="update-4"
+          label="Costo de la nave"
+          label-for="update-input-4"
+        >
+          <b-form-input id="update-input-4"
+            v-model="crew.ship_cost"
+            type="number"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="update-5"
+          label="Velocidad máxima de la nave"
+          label-for="update-input-5"
+        >
+          <b-form-input id="update-input-5"
+            v-model="crew.ship_max_speed"
+            type="number"
+          ></b-form-input>
+        </b-form-group>
       </b-modal>
 
     </b-container>
@@ -127,26 +292,16 @@ export default {
       perPage: 6,
       currentPage: 1,
       filter: null,
-      crew: null,
-      fields: ['age', 'first_name', 'detalle', 'editar', 'eliminar'],
-      crews: [
-        { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-        { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-        { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-        { age: 38, first_name: 'Jami', last_name: 'Carney' },
-        { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-        { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-        { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-        { age: 38, first_name: 'Jami', last_name: 'Carney' },
-        { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-        { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-        { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-        { age: 38, first_name: 'Jami', last_name: 'Carney' },
-        { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-        { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-        { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-        { age: 38, first_name: 'Jami', last_name: 'Carney' },
-      ],
+      responseMessage: '',
+      crew: {
+        name: null,
+        crew_quantity: null,
+        ship_name: null,
+        ship_cost: null,
+        ship_max_speed: null,
+      },
+      fields: ['id', 'name', 'ship_name', 'crew_quantity', 'ship_cost', 'ship_max_speed', 'detalle', 'editar', 'eliminar'],
+      crews: [],
     };
   },
   computed: {
@@ -154,25 +309,67 @@ export default {
       return this.crews.length;
     },
   },
+  mounted() {
+    this.getCrews();
+  },
   methods: {
+    onFiltered() {
+      this.currentPage = 1;
+    },
     setCrew(crew) {
-      console.log(crew.age);
+      this.crew = crew;
     },
-    createCrew() {
-      console.log('creating');
+    async getCrews() {
+      await this.axios.get('/crews/')
+        .then((response) => {
+          console.log(response.data);
+          this.crews = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    detailCrew(crew) {
-      console.log(crew.age);
+    async createCrew() {
+      await this.axios.post('/crews/', this.crew)
+        .then((response) => {
+          console.log(response);
+          this.crews.push(response.data);
+          this.responseMessage = 'Tripulacion creada correctamente';
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    updateCrew(crew) {
-      console.log(crew.age);
+    async updateCrew() {
+      await this.axios.put(`/crews/${this.crew.id}`, this.crew)
+        .then((response) => {
+          console.log(response);
+          this.responseMessage = 'Tripulacion actualizada correctamente';
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    deleteCrew(crew) {
-      console.log(crew.age);
+    async deleteCrew(crew) {
+      await this.axios.delete(`/crews/${crew.id}`, this.crew)
+        .then((response) => {
+          console.log(response);
+          this.crews.splice(this.crews.findIndex((actualCrew) => actualCrew.id === crew.id), 1);
+          this.responseMessage = 'Tripulacion eliminada correctamente';
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
     cleanCrew() {
       console.log('cleaning crew');
-      this.crew = null;
+      this.crew = {
+        name: null,
+        crew_quantity: null,
+        ship_name: null,
+        ship_cost: null,
+        ship_max_speed: null,
+      };
     },
   },
 };
